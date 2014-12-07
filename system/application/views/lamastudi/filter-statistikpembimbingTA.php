9a8e6a5a9041e9a49910b0de4fab712791cbee78
royyanbach <royyan.bachtiar@gmail.com>
2014-12-07 18:57:54 +0700

    ketinggalan file filternya statistik

---
+++ system/application/views/lamastudi/filter-statistikpembimbingTA.php
@@ -0,0 +1,29 @@
+<form id="filter-form" method="post" style="display: inline;float: left;padding: 0 1em 0 0; margin-bottom: 10px" action="<?php echo site_url('lamastudi/statistikPembimbingTA')?>">
+    Filter
+	<select onchange="document.forms['filter-form'].submit()" name="filter_statistik_tahun" style="min-width: 150px; height: 20px;">
+        <option value=""> - Pilih Tahun - </option>
+        <option value="all">Semua Tahun</option>
+        <?php
+        foreach ($tahun as $row_k) {
+            if($row_k->tahun == $this->session->userdata('filter_statistik_tahun'))
+                $tanda = " selected ";
+            else
+                $tanda = " ";
+            echo "<option $tanda value='".$row_k->tahun."'>".$row_k->tahun."</option>";
+        }
+        ?>
+    </select>
+    <select onchange="document.forms['filter-form'].submit()" name="filter_statistik_dosen" style="min-width: 150px; height: 20px;">
+        <option value=""> - Pilih Nama Dosen - </option>
+        <option value="all">Semua Dosen</option>
+        <?php
+        foreach ($dosen as $row_k) {
+            if($row_k['nip'] == $this->session->userdata('filter_statistik_dosen'))
+                $tanda = " selected ";
+            else
+                $tanda = " ";
+            echo "<option $tanda value='".$row_k['nip']."'>".ucwords(strtolower($row_k['nama_dosen']))."</option>";
+        }
+        ?>
+    </select>
+</form>
